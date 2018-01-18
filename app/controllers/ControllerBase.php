deb8c9220a201a7350eb8cd5b4859058a03ba15d
Jeffrey Y. Pogoy <jypogoy@gmail.com>
2018-01-18 10:24:02 +0800

    Continuation of flash session.

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -1,8 +1,30 @@
 <?php
 
 use Phalcon\Mvc\Controller;
+use Phalcon\Mvc\Dispatcher;
 
 class ControllerBase extends Controller
 {
+
+    // Executed before every found action.
+    public function beforeExecuteRoute(Dispatcher $dispatcher)
+    {
+        $flash = array();
+
+        foreach ($this->flashSession->getMessages() as $type => $message) {
+            if (!isset($flash[$type])) {
+                $flash[$type] = $message;
+            }
+            $flash[$type][] = $message;
+        }
+
+        $this->view->messages = $flash;
+    }
+
+    // Executed after every found action.
+    public function afterExecuteRoute(Dispatcher $dispatcher)
+    {
+        
+    }
 
 }
